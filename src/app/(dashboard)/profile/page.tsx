7926531ad7727effb41937ff640251852807bfbf
Siltstorm-GameHub <horn.sebastian@yahoo.de>
2026-05-31 13:47:40 +0200

    Delete src/app directory

--- src/app/(dashboard)/profile/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div className="p-6">
-      <h1 className="text-xl font-semibold text-white capitalize">profile</h1>
-      <p className="text-gray-400 mt-2 text-sm">Diese Seite wird als nächstes gebaut.</p>
-    </div>
-  );
-}
